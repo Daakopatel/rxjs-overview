a47dd1efe930d7ef772b1d96f03935952542adbe
Darshit Makasana <makasanadarshit092@gmail.com>
2023-11-06 10:40:16 +0530

    update

---
+++ src/combination.ts
@@ -0,0 +1,49 @@
+import { concat, interval, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { endWith, startWith, take } from "rxjs/operators";
+
+
+// start with 
+const numero$ = of(1, 2, 3).pipe(
+    startWith('a', 'b', 'c'),
+    endWith('x', 'y', 'z')
+);
+// numero$.subscribe( console.log );
+
+
+function labStart() {
+
+    const loadingDiv = document.createElement('div');
+    loadingDiv.classList.add('loading');
+    loadingDiv.innerHTML = 'loading data please wait...';
+
+    const body = document.querySelector('body');
+
+
+    const url = 'https://reqres.in';
+
+    ajax.getJSON(`${url}/api/users/2?delay=4`).pipe(
+        startWith('isloading')
+    ).subscribe(resp => {
+        if (resp === 'isloading') {
+            body.append(loadingDiv);
+        } else {
+            document.querySelector('.loading').remove();
+        }
+        console.log(resp);
+    });
+}
+
+// labStart();
+
+function concatt() {
+
+    const interval$ = interval(1000);
+
+    concat(
+        [1, 2, 3, 4],
+        of(1)
+    ).subscribe(console.log);
+}
+
+// concatt()
